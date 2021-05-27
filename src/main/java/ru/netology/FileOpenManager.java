e46c1f9778ddc6df465dc4a7f0627caa7ad133cc
Diana Krapivina <mr.ikoma@mail.ru>
2021-05-27 16:49:14 +0300

    Add FileOpenManager

--- src/main/java/ru/netology/FileOpenManager.java
+++ src/main/java/ru/netology/FileOpenManager.java
@@ -1,4 +1,31 @@
 package ru.netology;
 
+import java.util.*;
+
 public class FileOpenManager {
+    Map<String, String> map = new TreeMap<>();
+
+    public void registerApp(String file, String app) {
+        map.put(file, app);
+    }
+
+    public String getApp(String app) {
+        return map.get(app);
+    }
+
+    public void removeApp(String file) {
+        map.remove(file);
+    }
+
+    public Set<String> getAllExtension() {
+        Set<String> files = new TreeSet<>(Comparator.naturalOrder());
+        files.addAll(map.keySet());
+        return files;
+    }
+
+    public Set<String> getAllApp() {
+        Set<String> files = new TreeSet<>(Comparator.naturalOrder());
+        files.addAll(map.values());
+        return files;
+    }
 }
